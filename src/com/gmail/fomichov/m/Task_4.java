6f083d7b2c3a2e29e8deb6cbcb1d5cd541439998
Maksym <m.fomichov@gmail.com>
2017-07-14 16:39:41 +0300

    готово

--- src/com/gmail/fomichov/m/Task_4.java
+++ src/com/gmail/fomichov/m/Task_4.java
@@ -1,80 +1,31 @@
 package com.gmail.fomichov.m;
 
 /*
-Перегрузить функцию drawRectangle (задание 2) таким образом, что бы она могла принимать на вход только 1 параметр (ширина квадрата) и рисовать квадрат с равными сторонами
-Например 2
-Вывод программы:
- + +
- + +
- Например 3
- Вывод программы:
- + + +
- + + +
- + + +
- */
+аписать функцию getMax которая принимает на вход два аргумента в виде чисел. А возврыщает максимальное из этих двух.
+Так же, необходимо перегрузить эту функцию для работы с разными числовыми типами переменных (int, float)
+*/
 
-import java.util.Scanner;
 
-public class Task_3 {
+public class Task_4 {
 
     public static void main(String[] args) {
-        waitChoise();
+        System.out.println(getMax(5, 10));
+        System.out.println(getMax(5.2, 10.5));
     }
 
-    private static void waitChoise() {
-        System.out.println("Рисуем квадрат - 1, рисуем прямоугольник - 2");
-        switch (checkInt()) {
-            case 1:
-                System.out.println("Введите сторону квадрата:");
-                int side = checkInt();
-                drawRectangle(side);
-                break;
-            case 2:
-                System.out.println("Введите ширину прямоугольника:");
-                int width = checkInt();
-                System.out.println("Введите высоту прямоугольника:");
-                int height = checkInt();
-                drawRectangle(width, height);
-                break;
-            default:
-                System.out.println("Неверный выбор, повторите попытку");
-                waitChoise();
-                break;
+    private static int getMax(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
         }
     }
 
-    private static void drawRectangle(int side) {
-        for (int i = 0; i < side; i++) {
-            for (int j = 0; j < side; j++) {
-                System.out.print("+");
-            }
-            System.out.println();
+    private static double getMax(double a, double b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
         }
-    }
-
-    private static void drawRectangle(int width, int height) {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                System.out.print("+");
-            }
-            System.out.println();
-        }
-    }
-
-    // проверяем на целое число
-    private static int checkInt() {
-        Scanner sc = new Scanner(System.in);
-        int value;
-        while (true) {
-            try {
-                value = sc.nextInt();
-                sc.nextLine();
-                break;
-            } catch (Exception e) {
-                System.out.println("Только целое число!");
-                sc.nextLine();
-            }
-        }
-        return value;
     }
 }
